3c89d02d886140f24bf6546aa59552660b088ab8
omer kor <omerkorr@gmail.com>
2020-02-03 14:49:33 +0300

    kodlanmıs

--- StringCompareUygulamasi/StringCompareUygulamasi/Program.cs
+++ StringCompareUygulamasi/StringCompareUygulamasi/Program.cs
@@ -10,6 +10,24 @@ namespace StringCompareUygulamasi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("kullanıcı adı belirleyiniz");
+            string kullanici_adi = Console.ReadLine();
+           PAROLAUYUSMADIGINDA:
+            Console.WriteLine("parola belirleyiniz");
+            string parola1 = Console.ReadLine();
+            Console.WriteLine("parolanızı tekrar giriniz");
+            string parola2 = Console.ReadLine();
+            int kontrol = string.Compare(parola1, parola2); //parola1 ve parola2 yi karsılastırır aynıysa sıfır der ve kontrol degiskenine sıfır tanımlanır.
+            if (kontrol==0)
+            {
+                Console.WriteLine("Kayıt işlemi başarılıdır");
+            }
+            else
+            {
+                Console.WriteLine("parolalar uyuşmuyor");
+                goto PAROLAUYUSMADIGINDA;
+            }
+            Console.ReadKey();
         }
     }
 }
